b18fdb134342c8ab7cd3ef5474e92563d1d0bb88
dotFour <rubaiyat9@gmail.com>
2017-03-27 01:02:20 +0600

    search page updated

---
+++ booking.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title> TMS - Add Booking </title>
+</head>
+
+<body>
+	<fieldset>
+	<form>
+		<table>
+			<thead></thead>
+			<tbody>
+				<tr>
+					<td> <p> First Name: </p> </td>
+					<td> <input type="text" name="fnfield"/>
+				</tr>
+			</tbody>
+	</form>
+	</fieldset>
+</body>
