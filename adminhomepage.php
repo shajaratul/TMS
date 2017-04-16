47013cf039fd6d660dafab1391758a3f91ae7c0a
dotFour <rubaiyat9@gmail.com>
2017-04-16 23:46:13 +0600

    admin home page initial

---
+++ adminhomepage.php
@@ -0,0 +1,78 @@
+<html>
+<head>
+	<title> TMS - Admin page </title>
+	<style>
+	
+	ul {
+		list-style-type: none;
+		margin: 0px;
+		padding: 0px;
+		width: 20%;
+		background-color: #f1f1f1;
+		position: fixed;
+		height: 100%;
+		overflow: auto;
+	}
+
+	
+	li a {
+		display: block;
+		color: #000;
+		padding: 8px 16px;
+		text-decoration: none;
+	}
+	
+	li a.active {
+		background-color: #3366ff;
+		color: white;
+	}
+	
+	li a.active:hover {
+		background-color: #33ccff;
+		color: white;
+	}
+	
+	li a:hover:not(.active) {
+		background-color: #555;
+		color: white;
+	}
+	
+	div #dashboard {
+		margin-left: 20%;
+		padding: 1px 16px;
+		height: 100%;
+	}
+	
+	div #site-header {
+		position: fixed;
+		background-color: grey;
+		color: white;
+	}
+	</style>
+</head>
+
+<body>
+	<div id="site-header">
+		<h1> Travel.com </h1>
+	</div>
+	<div>		
+
+	<div>	
+		<ul>
+			<li><a href="#"> Members</a></li>
+			<li><a href="#"> Places </a></li>
+			<li><a href="#"> Add Place </a></li>
+			<li><a href="#"> Delete Place </a></li>
+			<li><a href="#"> Modify Place</a></li>
+			<li><a href="#"> Holiday Package </a></li>
+			<li><a href="#"> Bookings </a></li>
+		</ul>
+	</div>
+	
+	<div id="dashboard">
+		<p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p><p> Lorem Ipsum </p>
+	</div>
+	
+	</div>
+</body>
+</html>
