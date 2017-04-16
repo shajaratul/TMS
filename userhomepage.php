f75032d39d327f9b5d9ff5765655801bc3de2747
dotFour <rubaiyat9@gmail.com>
2017-04-17 03:42:05 +0600

    changed homepage to the new one

---
+++ userhomepage.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+	<title> TMS - User's homepage </title>
+</head>
+
+<body>
+	<div>
+	
+		<h2> Hi, user! </h2>
+		
+		<div>
+		
+			<fieldset>
+				<legend> Your Details </legend>
+				<br/>
+				<br/>
+				<br/>
+				<br/>
+				<br/>
+			
+			</fieldset>
+			
+			<fieldset>
+				<legend> Your Trips </legend>
+				
+				<div>
+					<h3> Your Ongoing Trip </h3>
+					<hr/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+				</div>
+				
+				<div>
+					<h3> Your Past Trips </h3>
+					<hr/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+				</div>
+				
+				
+			</fieldset>
+		</div>
+	
+	</div>
+</body>
+</html>
